873fc7c24a0626995a93a743142a5af6ecbcaf09
Tomás Arribas <tomas@cactilio.com>
2026-01-01 15:30:09 +0100

    fix: update multiple queue example to use decorator

--- examples/multiple-queues/src/notifications/notifications.queue.ts
+++ examples/multiple-queues/src/notifications/notifications.queue.ts
@@ -1,16 +1,14 @@
-import Agenda from 'agenda-ts';
-import { Every, InjectQueue, Queue } from 'agenda-nest';
+import { Every, OnJobComplete, Queue } from 'agenda-nest';
 
 @Queue('notifications')
 export class NotificationsQueue {
-  constructor(@InjectQueue('notifications') queue: Agenda) {
-    queue.on('complete:sendNotification', () => {
-      console.log('notification sent');
-    });
-  }
-
   @Every('1 minute')
   sendNotification() {
     console.log('Sending notification to all users');
+  }
+
+  @OnJobComplete('sendNotification')
+  onNotificationSent() {
+    console.log('notification sent');
   }
 }
